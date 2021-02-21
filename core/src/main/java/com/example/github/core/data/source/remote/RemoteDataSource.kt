c31693df8b8c3b5a3b380ab7e20b6f6d8a61352f
Fikri Yurcel Milano <fikrimilano1@gmail.com>
2021-02-21 09:52:52 +0700

    Update Key

--- core/src/main/java/com/example/github/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/example/github/core/data/source/remote/RemoteDataSource.kt
@@ -1,10 +1,10 @@
 package com.example.github.core.data.source.remote
 
 import android.util.Log
-import com.example.github.core.BuildConfig
 import com.example.github.core.data.source.remote.network.ApiResponse
 import com.example.github.core.data.source.remote.network.ApiService
 import com.example.github.core.data.source.remote.response.UserResponse
+import com.example.github.core.utils.ApiConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -17,7 +17,7 @@ class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
 
     suspend fun getListUser(): Flow<ApiResponse<List<UserResponse>>> = flow {
         try {
-            val response = apiService.getListUser(BuildConfig.API_KEY)
+            val response = apiService.getListUser(ApiConfig.API_KEY)
 
             if (response.isNotEmpty())
                 emit(ApiResponse.Success(response))
@@ -32,7 +32,7 @@ class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
 
     suspend fun getDetailUser(username: String): Flow<ApiResponse<UserResponse>> = flow {
         try {
-            val response = apiService.getDetailUser(BuildConfig.API_KEY, username)
+            val response = apiService.getDetailUser(ApiConfig.API_KEY, username)
 
             if (response.username != null)
                 emit(ApiResponse.Success(response))
@@ -47,7 +47,7 @@ class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
 
     suspend fun getSearchListUser(username: String): Flow<ApiResponse<List<UserResponse>>> = flow {
         try {
-            val response = apiService.getSearchListUser(BuildConfig.API_KEY, username, 5)
+            val response = apiService.getSearchListUser(ApiConfig.API_KEY, username, 5)
 
             if (response.listUser.isNotEmpty())
                 emit(ApiResponse.Success(response.listUser))
---
+++ core/src/main/java/com/example/github/core/utils/ApiConfig.kt
@@ -0,0 +1,5 @@
+package com.example.github.core.utils
+
+object ApiConfig {
+    var API_KEY = "token 9b2f979fae220f483b342ec91ba16c6226adb27a"
+}
